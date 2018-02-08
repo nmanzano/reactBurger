893531f872ca352472789dd5aa9b9310fb7400ac
Noni <noni.manzano@gmail.com>
2018-02-07 20:57:09 -0600

    add side drawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -10,13 +10,19 @@ class Layout extends Component {
   }
 
 sideDrawerClosedHandler = () => {
-  this.setState({showSideDrawer: false})
+  this.setState({showSideDrawer:false});
+}
+
+sideDrawerToggleHandler = () => {
+  this.setState((prevState) => {
+    return {showSideDrawer: !prevState.showSideDrawer};
+  });
 }
 
   render () {
     return (
       <Aux>
-        <Toolbar/>
+        <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>
         <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler}/>
         <main className={classes.Content}>
           {this.props.children}
