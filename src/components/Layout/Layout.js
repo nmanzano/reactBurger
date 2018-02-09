aa59a0cd112883122ca7820457135459a6ebe8ed
Noni <noni.manzano@gmail.com>
2018-02-08 22:40:18 -0600

    added icon in menu drop down

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -4,9 +4,10 @@ import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
+
 class Layout extends Component {
   state = {
-    showSideDrawer: true
+    showSideDrawer: false
   }
 
 sideDrawerClosedHandler = () => {
